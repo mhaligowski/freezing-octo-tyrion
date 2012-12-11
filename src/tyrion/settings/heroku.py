c1d3eab8798078c4dbaa6639993be6ed9b50d32e
Mateusz Haligowski <mhaligowski@gmail.com>
2012-12-11 23:35:43 +0100

    static configuration attempt

--- src/tyrion/settings/heroku.py
+++ src/tyrion/settings/heroku.py
@@ -31,6 +31,8 @@ EMAIL_HOST_PASSWORD = os.environ['MAILGUN_SMTP_PASSWORD']
 EMAIL_USE_TLS = False
 
 # static files
+STATIC_ROOT = (os.path.join(head, 'static'))
+
 STATICFILES_DIRS = (
 	os.path.join(head, 'www'),
 )
